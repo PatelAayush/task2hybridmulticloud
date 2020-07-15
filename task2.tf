9d6e92889a140636d67417a049c8bfaa014d6e57
Aayush Patel <49674833+PatelAayush@users.noreply.github.com>
2020-07-15 20:08:50 +0530

    Update task2.tf

--- task2.tf
+++ task2.tf
@@ -2,8 +2,6 @@ provider "aws" {
   version = "~> 2.0"
   region  = "ap-south-1"
   profile = "default"
-  access_key = "AKIAI2ZDAUT2SXC2NINA"
-  secret_key = "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"
 }
 
 
